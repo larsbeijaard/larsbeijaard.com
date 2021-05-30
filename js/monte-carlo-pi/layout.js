07eb9693553fad7fd83a6a6e4b7a7929ede1dde5
Lars Beijaard <larsbeijaard@outlook.com>
2021-05-30 15:14:36 +0200

    Changed name + added a grid view

--- js/monte-carlo-pi/layout.js
+++ js/monte-carlo-pi/layout.js
@@ -12,6 +12,29 @@ function drawCanvas() {
     ctx.stroke();
 }
 
+function drawGridLines() {
+    const offset = 500 - width;
+    const lineOffset = 10;
+
+    // Vertical lines.
+    for (let i = 2; i <= width / lineOffset; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = '#ebebeb'
+        ctx.moveTo((offset / 2) + (lineOffset * i), 0 + 1);
+        ctx.lineTo((offset / 2) + (lineOffset * i), canvas.height - offset - 1);
+        ctx.stroke();
+    }
+
+    // Horizontal lines.
+    for (let i = 0; i <= height / lineOffset - 2; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = '#ebebeb'
+        ctx.moveTo(offset + 1, (offset / 2) + (lineOffset * i));
+        ctx.lineTo(canvas.width - 1, (offset / 2) + (lineOffset * i));
+        ctx.stroke();
+    }
+}
+
 function drawSideMarks() {
     // Mark zero.
     ctx.fillText('0.0', 0, canvas.height);
@@ -31,11 +54,12 @@ function drawArc() {
     const offset = 500 - width;
 
     ctx.beginPath();
-    ctx.strokeStyle = '#FF0000'; // Todo: change this value to the given value in the stats.
+    ctx.strokeStyle = '#FF0000';
     ctx.arc(offset, canvas.height - offset, width, -0.5 * Math.PI, 0);
     ctx.stroke();
 }
 
 drawCanvas();
+drawGridLines();
 drawSideMarks();
 drawArc();
