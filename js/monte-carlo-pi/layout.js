978996d1e3b915881770813c69007a2285261965
Lars Beijaard <larsbeijaard@outlook.com>
2021-05-30 15:40:06 +0200

    Rewrote the grid drawing function

--- js/monte-carlo-pi/layout.js
+++ js/monte-carlo-pi/layout.js
@@ -1,38 +1,42 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = 480;
-const height = 480;
+const totalWidth = canvas.width;
+const totalHeight = canvas.height;
 
-function drawCanvas() {
-    const markOffset = 500 - width;
+// Decrease the grid with a bit to keep space for the side/bottom marks (0.0, 0.1, etc).
+const gridWidth = 480;
+const gridHeight = 480;
+
+function drawGridBorders() {
+    const offset = totalWidth - gridWidth;
 
     ctx.beginPath();
-    ctx.rect(markOffset, 0, width, height);
+    ctx.strokeStyle = '#000000';
+    ctx.rect(offset, 0, gridWidth, gridHeight);
     ctx.stroke();
 }
 
 function drawGridLines() {
-    const offset = 500 - width;
-    const lineOffset = 10;
+    const offset = totalWidth - gridWidth;
+    const lineSpacing = 10;
+    
+    ctx.beginPath();
+    ctx.strokeStyle = '#ebebeb';
 
-    // Vertical lines.
-    for (let i = 2; i <= width / lineOffset; i++) {
-        ctx.beginPath();
-        ctx.strokeStyle = '#ebebeb'
-        ctx.moveTo((offset / 2) + (lineOffset * i), 0 + 1);
-        ctx.lineTo((offset / 2) + (lineOffset * i), canvas.height - offset - 1);
-        ctx.stroke();
+    // Draw the grid lines.
+    for (let i = 1; i < gridWidth / lineSpacing; i++) {
+        // Horizontal lines.
+        ctx.moveTo(offset + 1, lineSpacing * i);
+        ctx.lineTo(canvas.width - 1, lineSpacing * i);
+
+        // Vertical lines.
+        ctx.moveTo(lineSpacing * i + offset, 1);
+        ctx.lineTo(lineSpacing * i + offset, canvas.height - offset - 1);
+
     }
 
-    // Horizontal lines.
-    for (let i = 0; i <= height / lineOffset - 2; i++) {
-        ctx.beginPath();
-        ctx.strokeStyle = '#ebebeb'
-        ctx.moveTo(offset + 1, (offset / 2) + (lineOffset * i));
-        ctx.lineTo(canvas.width - 1, (offset / 2) + (lineOffset * i));
-        ctx.stroke();
-    }
+    ctx.stroke();
 }
 
 function drawSideMarks() {
@@ -51,15 +55,15 @@ function drawSideMarks() {
 }
 
 function drawArc() {
-    const offset = 500 - width;
+    const offset = 500 - gridWidth;
 
     ctx.beginPath();
     ctx.strokeStyle = '#FF0000';
-    ctx.arc(offset, canvas.height - offset, width, -0.5 * Math.PI, 0);
+    ctx.arc(offset, canvas.height - offset, gridWidth, -0.5 * Math.PI, 0);
     ctx.stroke();
 }
 
-drawCanvas();
+drawGridBorders();
 drawGridLines();
 drawSideMarks();
 drawArc();
