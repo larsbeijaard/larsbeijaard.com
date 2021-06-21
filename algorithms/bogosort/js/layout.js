0307dd26579b039d3142e36f0b0cf564a3079ac3
Lars Beijaard <larsbeijaard@outlook.com>
2021-06-21 23:36:14 +0200

    Set the gridHeight variable availability to public

--- algorithms/bogosort/js/layout.js
+++ algorithms/bogosort/js/layout.js
@@ -1,19 +1,21 @@
 import { createGrid, setSideMarks } from '../../setups/grid.js'; 
 import { generateSticks } from '../../setups/sticks.js';
 
-const _gridHeight = 375;
+const gridHeight = 375;
 const _gridColor = '#000000';
 
 const _markFrom = 0;
 const _markTo = 100;
-const _markCount = 3;
+const _markCount = 5;
 
-const _stickCount = 10;
+const _stickCount = 3;
 const _stickLow = 10;
 const _stickHigh = 100;
 const _stickColor = '#368FF5';
 
-createGrid(_gridHeight, _gridColor);
-setSideMarks(_markFrom, _markTo, _markCount, _gridHeight);
+createGrid(gridHeight, _gridColor);
+setSideMarks(_markFrom, _markTo, _markCount, gridHeight);
 
 generateSticks(_stickLow, _stickHigh, _stickCount, _stickColor);
+
+export { gridHeight };
