8e127759fe8bb055c1de9eb92e0cf2ee69dd746d
Lars Beijaard <larsbeijaard@outlook.com>
2021-06-29 16:09:57 +0200

    Change the algorithm iteration speed

--- larsbeijaard/js/sorting/controlSpeed.js
+++ larsbeijaard/js/sorting/controlSpeed.js
@@ -1,4 +1,7 @@
+// TODO: find a better way to keep track of the current speed of algorithms.
+
 var speeds = {
+    // algorithm : speed
     0: 1,
     1: 1,
     2: 1
@@ -17,9 +20,31 @@ function initializeSpeedListener() {
 
 function changeSpeed(id) {
     const speedElement = document.getElementsByClassName(`speed ${id}`)[0];
-    var speed = ++speeds[id];
+    var speed = getNextSpeed(id);
 
+    // Update the HTML element with the new speed text.
+    // For example: if the new speed is 2, the text will be put as 'x2.0'.
     speedElement.innerHTML = `x${speed}.0`;
+}
+
+function getNextSpeed(speedsID) {
+    const maxSpeed = 4;
+    
+    for (const [key, value] in speeds) {
+        // Find the key that matches the speeds ID.
+        if (speedsID === key) {
+            var currentSpeed = speeds[speedsID];
+            // Calculate the new speed value.
+            var newSpeed = currentSpeed < maxSpeed ? ++currentSpeed : currentSpeed = 1;
+
+            // Update the kvp in the speeds variable.
+            speeds[speedsID] = newSpeed;
+
+            return newSpeed;
+        }
+    }
+
+    return 1;
 }
 
 initializeSpeedListener();
