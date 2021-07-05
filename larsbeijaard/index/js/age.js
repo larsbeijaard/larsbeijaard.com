414719930b9676d6e8a89984009916fc66cc3dfd
Lars Beijaard <larsbeijaard@outlook.com>
2021-07-05 11:34:05 +0200

    Added a comment

--- larsbeijaard/index/js/age.js
+++ larsbeijaard/index/js/age.js
@@ -3,6 +3,7 @@ function computeAge() {
     const currentYear = currentDate.getFullYear();
     const birthYear = 2002;
 
+    // Add one year to my age if my birth date has already been passed.
     const age = currentDate > new Date(currentYear, 0, 5)
                 ? currentYear - birthYear
                 : currentYear - birthYear - 1;
